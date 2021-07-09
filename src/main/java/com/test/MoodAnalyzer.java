f6531d6ba6d851c20cc8abf017e574350d0da33e
Minal <patilminalp@gmail.com>
2021-07-09 15:45:14 +0530

    Added:Check Mood Happy or Sad using Test-Cases

--- src/main/java/com/test/MoodAnalyzer.java
+++ src/main/java/com/test/MoodAnalyzer.java
@@ -1,5 +1,14 @@
 package com.test;
 
 public class MoodAnalyzer {
+//Check 
+	public String analyseMood(String message) {
+
+		if (message.toLowerCase().contains("sad"))
+			return "SAD";
+
+		return "HAPPY";
+
+	}
 
 }
--- src/test/java/com/test/MoodAnalyzerTest.java
+++ src/test/java/com/test/MoodAnalyzerTest.java
@@ -1,5 +1,27 @@
 package com.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+	// Test-Case for SadMood
+	@Test
+	public void givenMessage_whenSad_ShouldReturnSad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String actual = moodAnalyzer.analyseMood("I am Sad");
+		String expected = "SAD";
+		Assert.assertEquals(expected, actual);
+
+	}
+
+	// Test-Case for HappyMood
+	@Test
+	public void givenMessage_whenHappy_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String actual = moodAnalyzer.analyseMood("I am Happy");
+		String expected = "HAPPY";
+		Assert.assertEquals(expected, actual);
+
+	}
 
 }
